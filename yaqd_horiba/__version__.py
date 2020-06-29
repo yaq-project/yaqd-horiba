a139c260bd76c83511d357bbb195b42c2732e5d6
Blaise Thompson <blaise@untzag.com>
2020-06-29 16:58:27 +0000

    Update __version__.py

--- yaqd_horiba/__version__.py
+++ yaqd_horiba/__version__.py
@@ -1,13 +1,8 @@
 """Define version."""
 
 
-# --- import --------------------------------------------------------------------------------------
-
-
 import pathlib
-
-
-# ---- define -------------------------------------------------------------------------------------
+import subprocess
 
 
 here = pathlib.Path(__file__).resolve().parent
@@ -16,24 +11,18 @@ here = pathlib.Path(__file__).resolve().parent
 __all__ = ["__version__", "__branch__"]
 
 
-# --- version -------------------------------------------------------------------------------------
-
-
 # read from VERSION file
 with open(str(here / "VERSION")) as f:
     __version__ = f.read().strip()
 
-
-# add git branch, if appropriate
-p = here.parent / ".git"
-if p.is_file():
-    with open(str(p)) as f:
-        p = p.parent / f.readline()[8:].strip()  # Strip "gitdir: "
-p = p / "HEAD"
-if p.exists():
-    with open(str(p)) as f:
-        __branch__ = f.readline().rstrip().split(r"/")[-1]
-    if __branch__ != "master":
-        __version__ += "+" + __branch__
-else:
+try:
+    __branch__ = (
+        subprocess.run(["git", "branch", "--show-current"], capture_output=True, cwd=here)
+        .stdout.strip()
+        .decode()
+    )
+except:
     __branch__ = ""
+
+if __branch__:
+    __version__ += "+" + __branch__
