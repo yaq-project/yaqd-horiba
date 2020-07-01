a0de1083e3b602846934868c7746373c313e46b1
Kyle Sunden <git@ksunden.space>
2020-07-01 23:44:26 +0000

    Avro

--- yaqd_horiba/_microhr.py
+++ yaqd_horiba/_microhr.py
@@ -5,8 +5,6 @@ import usb.core  # type: ignore
 
 from yaqd_core import ContinuousHardware, logging
 
-from .__version__ import __branch__
-
 
 __all__ = ["MicroHR"]
 
@@ -33,14 +31,6 @@ IS_BUSY = 5
 
 class MicroHR(ContinuousHardware):
     _kind = "micro-hr"
-    _version = "1.0.0" + f"+{__branch__}" if __branch__ else ""
-    traits = ["has-turret", "is-homeable"]
-    defaults = {
-        "make": "Horiba Jobin-Yvon",
-        "model": "MicroHR",
-        "force_init": True,
-        "emulate": False,
-    }
 
     def __init__(self, name, config, config_filepath):
         # TODO: Support multiple monos on the same machine
@@ -54,8 +44,8 @@ class MicroHR(ContinuousHardware):
         self._dev._langids = (LANG_ID_US_ENGLISH,)
         self.description = self._dev.product
         self.serial = self._dev.serial_number
-        self._gratings = [{"lines_per_mm": 1200}, {"lines_per_mm": 1200}]
-        [g.update(c) for g, c in zip(self._gratings, config.get("gratings"))]
+        self._gratings = config["gratings"]
+        self._units = "nm"
 
         self.home()
 
@@ -64,16 +54,16 @@ class MicroHR(ContinuousHardware):
 
         if self._busy:
             await self._not_busy_sig.wait()
-        self.logger.debug(self._turret)
+        self.logger.debug(self._state["turret"])
         self._dev.ctrl_transfer(
             B_REQUEST_OUT,
             BM_REQUEST_TYPE,
             wIndex=SET_TURRET,
-            data_or_wLength=struct.pack("<i", self._turret),
+            data_or_wLength=struct.pack("<i", self._state["turret"]),
         )
         self._busy = True
         await self._not_busy_sig.wait()
-        self.set_position(self._destination)
+        self.set_position(self._state["destination"])
         # _, lo, hi = self.controller.IsTargetWithinLimits(0, 0)
         # self._limits = [(lo, hi)]
 
@@ -85,16 +75,17 @@ class MicroHR(ContinuousHardware):
 
     def set_turret(self, index):
         self._busy = True
-        self.logger.debug(self._turret, index)
-        if index != self._turret:
-            self._turret = index
+        self.logger.debug(self._state["turret"], index)
+        if index != self._state["turret"]:
+            self._state["turret"] = index
             loop = asyncio.get_event_loop()
             loop.create_task(self._reset_position())
+            self._state["hw_limits"] = (0, 1580 * 1200 / self._gratings[index])
 
     def _set_position(self, position):
         # Mono assumes 1200 lines/mm, adjust accordingly
         self.logger.debug(position)
-        position = position * self._gratings[self._turret]["lines_per_mm"] / 1200.0
+        position = position * self._gratings[self._state["turret"]] / 1200.0
         self.logger.debug(position)
         self._dev.ctrl_transfer(
             B_REQUEST_OUT,
@@ -109,27 +100,14 @@ class MicroHR(ContinuousHardware):
                 B_REQUEST_IN, BM_REQUEST_TYPE, wIndex=IS_BUSY, data_or_wLength=4
             )
             self._busy = struct.unpack("<i", busy)[0]
-            self._position = struct.unpack(
+            self._state["position"] = struct.unpack(
                 "<f",
                 self._dev.ctrl_transfer(
                     B_REQUEST_IN, BM_REQUEST_TYPE, wIndex=READ_WAVELENGTH, data_or_wLength=4
                 ),
             )[0]
-            self._position = self._position / (
-                self._gratings[self._turret]["lines_per_mm"] / 1200.0
+            self._state["position"] = self._state["position"] / (
+                self._gratings[self._state["turret"]] / 1200.0
             )
             await asyncio.sleep(0.01)
             await self._busy_sig.wait()
-
-    def get_state(self):
-        state = super().get_state()
-        state["turret"] = self._turret
-        return state
-
-    def _load_state(self, state):
-        super()._load_state(state)
-        self._turret = state.get("turret", 0)
-
-
-if __name__ == "__main__":
-    MicroHR.main()
