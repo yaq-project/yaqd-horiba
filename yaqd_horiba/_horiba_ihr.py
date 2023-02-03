0bcb4d79636e3e3534e4e6ced41290088fa36291
Blaise Thompson <blaise@untzag.com>
2023-02-03 10:56:17 -0600

    mypy

--- yaqd_horiba/_horiba_ihr.py
+++ yaqd_horiba/_horiba_ihr.py
@@ -31,12 +31,12 @@ class HoribaIHR320(HoribaMono):
         while True:
             await self._not_busy_sig.wait()
             for i in range(2):
-                if startup_state["mirrors_dest"][i] != self._get_mirror(i) 
+                if startup_state["mirrors_dest"][i] != self._get_mirror(i):
                     self._state["mirrors_dest"][i] = startup_state["mirrors_dest"][i]
                     await self._reset_position()
                     continue
             for in range(4):
-                if startup_state["slits_dest"][i] != self._get_slit(i)
+                if startup_state["slits_dest"][i] != self._get_slit(i):
                     self._state["slits_dest"][i] = startup_state["slits_dest"][i]
                     await self._reset_position()
                     continue
